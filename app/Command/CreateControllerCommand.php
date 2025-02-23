0f2889c177b05853f9aaeca2fcbf98f47cdca9d7
Nikolad <archy@pop-os.local>
2025-02-23 11:45:41 +0100

    feat(male:controller): validation

--- app/Command/CreateControllerCommand.php
+++ app/Command/CreateControllerCommand.php
@@ -2,12 +2,13 @@
 
 namespace NikoGin\Command;
 
+use NikoGin\Core\Support\Validator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 #[AsCommand(
     name: 'make:controller',
@@ -23,7 +24,24 @@ class CreateControllerCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $name = $input->getArgument('name');
-        $type = $input->getArgument('type');
+        $type = strtolower($input->getArgument('type'));
+
+        $helper = $this->getHelper('question');
+
+        while (!in_array($type, Validator::ALLOWED_TYPES, true)) {
+            $output->writeln('<error>Invalid type. Allowed types: rest, menu, submenu.</error>');
+
+            $question = new ChoiceQuestion(
+                'Please choose a valid type: ',
+                Validator::ALLOWED_TYPES,
+                0
+            );
+            $question->setErrorMessage('Invalid choice.');
+
+            $type = $helper->ask($input, $output, $question);
+        }
+
+
 
         $output->writeln(sprintf('<info>Creating Controller:</info> %s (%s)', $name, $type));
 
