ef80d98bbc5f8f98d15eb52aaf81f2388b4f20e8
Nikolad <archy@pop-os.local>
2025-03-13 20:50:30 +0100

    ref(command): input

--- app/Command/CreateListenerCommand.php
+++ app/Command/CreateListenerCommand.php
@@ -24,6 +24,7 @@ class CreateListenerCommand extends Command
         $this->addArgument('name', InputArgument::REQUIRED, 'The name of the listener');
         $this->addArgument('action', InputArgument::REQUIRED, 'The action of the listener');
         $this->addArgument('directory', InputArgument::REQUIRED, 'The plugin directory (e.g., myplugin)');
+        $this->addArgument('type', InputArgument::OPTIONAL, 'The type of the listener action/filter');
         $this->addArgument('args', InputArgument::OPTIONAL, 'The number of args for callback');
         $this->addArgument('priority', InputArgument::OPTIONAL, 'The priority level of the listener');
     }
@@ -33,16 +34,27 @@ class CreateListenerCommand extends Command
         $name = $input->getArgument('name');
         $action = $input->getArgument('action');
         $directory = $input->getArgument('directory');
+        $type = $input->getArgument('type');
         $args = $input->getArgument('args');
         $priority = $input->getArgument('priority');
 
+        if($type !== "action" && $type !== "filter")
+        {
+            $output->writeln('<error>Invalid type</error>');
+            return Command::FAILURE;
+        }
+
         $data = ['name'     => $name,
-                 'action'   => $action,
+                 'listener'   => $action,
                  'dir'      => $directory,
+                 'type'     => $type,
                  'args'     => $args,
                  'priority' => $priority];
 
         $dir = $this->listenerBuilder->create($data);
+
+        $output->writeln(sprintf('<info>Creating Listener in:</info> %s', $dir));
+        $output->writeln(sprintf("<info> Listener {$type} name:</info> %s", $name));
 
 
         return Command::SUCCESS;
